01f428953bed8eaf87b2cdda11edc5f14a13ad4d
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2024-09-03 11:32:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 /* eslint-disable no-unused-vars */
-//import logo from './logo.svg';
 import './App.css';
 import Title from './title';
 import AddBook from './addBook';
 import React, { useState, useEffect } from 'react'
-//import axios from "axios"
-//import { useNavigate } from 'react-router-dom';
+
 
 function App() {
 // eslint-disable-next-line no-unused-vars
   const [data, setData] = useState([{}])
-
-  /*
-
-  const navigate = useNavigate();
-
-  const handleNavigate = () => {
-    navigate('/about');
-  };
-
-  */
-
 
 
 // eslint-disable-next-line no-unused-vars
